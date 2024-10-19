a26234d393b23abfea04865975e2796344e78eb6
Adam <Adam.ja.com@op.pl>
2024-10-19 19:11:48 +0200

    second way connection added

--- src/main/java/com/brightpath/learnify/domain/auth/permission/PermissionsAccess.java
+++ src/main/java/com/brightpath/learnify/domain/auth/permission/PermissionsAccess.java
@@ -1,9 +1,14 @@
 package com.brightpath.learnify.domain.auth.permission;
 
+import com.brightpath.learnify.exception.notfound.ResourceType;
+
 import java.util.List;
+import java.util.UUID;
 
 public record PermissionsAccess(
         PermissionLevel permissionLevel,
-        List<Permission> permissions
+        List<Permission> permissions,
+        ResourceType resourceType,
+        UUID resourceId
 ) {
 }
