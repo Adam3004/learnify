6ae5009b05a523f3f1ef540cdab4eaecc1c49d2d
Adam <Adam.ja.com@op.pl>
2024-10-19 23:05:41 +0200

    permission entities

---
+++ src/main/java/com/brightpath/learnify/persistance/auth/permissions/PermissionsAccessEntity.java
@@ -0,0 +1,42 @@
+package com.brightpath.learnify.persistance.auth.permissions;
+
+import com.brightpath.learnify.domain.auth.permission.PermissionLevel;
+import com.brightpath.learnify.domain.common.ResourceType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "permissions_access")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PermissionsAccessEntity {
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    private UUID id;
+
+    @Column(name = "permission_level", nullable = false)
+    private PermissionLevel permissionLevel;
+
+    @OneToMany
+    @JoinColumn(name = "permissions")
+    private Set<PermissionEntity> permissions;
+
+    @Column(name = "resource_type", nullable = false)
+    private ResourceType resourceType;
+
+    @Column(name = "resource_id", nullable = false)
+    private UUID resourceId;
+}
