27c3aac96263916bb0cf1fb0a1e1fec8ea36fece
krisx303 <57154118+krisx303@users.noreply.github.com>
2024-11-08 00:26:26 +0100

    Support for simple workspace view with permissions UI

--- src/main/java/com/brightpath/learnify/persistance/quiz/QuizRepository.java
+++ src/main/java/com/brightpath/learnify/persistance/quiz/QuizRepository.java
@@ -7,6 +7,14 @@ import java.util.List;
 import java.util.UUID;
 
 public interface QuizRepository extends JpaRepository<QuizEntity, UUID> {
+
+    @Query("""
+            SELECT u
+            FROM QuizEntity u
+            WHERE (:workspaceId IS NULL OR u.workspace.id = :workspaceId)
+            """)
+    List<QuizEntity> searchQuizzes(UUID workspaceId);
+
     @Query("SELECT e FROM QuizEntity e ORDER BY COALESCE(e.lastTryDate, e.createdAt) DESC LIMIT 4")
     List<QuizEntity> findTop4RecentQuizzes();
 }
